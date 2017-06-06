9e9f8f89ba2806de6782ed3ed975fda76e93e911
Samuel Finocchio (Informaticage) <samuelfinocchioit@gmail.com>
2017-06-06 19:51:58 +0200

    Added facebook login passport configuration

--- config/passport.js
+++ config/passport.js
@@ -41,5 +41,17 @@ module.exports = function(passport) {
     ));
 
     // Facebook Strategy Login
-
+    passport.use(new FacebookStrategy({
+      clientID: apiCredentials.FACEBOOK_APP_ID,
+      clientSecret: apiCredentials.FACEBOOK_APP_SECRET,
+      callbackURL: apiCredentials.FACEBOOK_CALLBACK_URL
+    },
+    function(accessToken, refreshToken, profile, done) {
+      /*User.findOrCreate(..., function(err, user) {
+        if (err) { return done(err); }
+        done(null, user);
+      });*/
+      console.log(JSON.stringify(profile));
+    }
+  ));
 };
