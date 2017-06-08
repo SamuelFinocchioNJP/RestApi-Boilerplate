f1338620836342b2f4a8c46cd8adeddffb948b87
Samuel Roberto <samuel.roberto997@gmail.com>
2017-06-08 18:54:39 +0200

    Fixed errors

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const passport = require('passport');
 const morgan = require('morgan');
 const session = require('express-session');
 
+const swaggerTools = require('swagger-tools');
+const jsyaml = require('js-yaml');
+const fs = require('fs');
+
 const mainConfig = require('./config/main');
 
 // Initialization
@@ -39,11 +43,32 @@ app.get('/', function (req, res, next) {
 });
 
 // Using custom routes
-const demo = require('./routes/demo');
-app.use('/custom', demo);
+// swaggerRouter configuration
+var options = {
+    swaggerUi: './swagger.json',
+    controllers: './controllers',
+    useStubs: process.env.NODE_ENV === 'development' ? true : false // Conditionally turn on stubs (mock mode)
+};
 
-const user = require ('./routes/user');
-app.use(user);
+// The Swagger document (require it, build it programmatically, fetch it from a URL, ...)
+var spec = fs.readFileSync('./api/swagger.yaml', 'utf8');
+var swaggerDoc = jsyaml.safeLoad(spec);
+
+// Initialize the Swagger middleware
+swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
+    // Interpret Swagger resources and attach metadata to request - must be first in swagger-tools middleware chain
+    app.use(middleware.swaggerMetadata());
+
+    // Validate Swagger requests
+    app.use(middleware.swaggerValidator());
+
+    // Route validated requests to appropriate controller
+    app.use(middleware.swaggerRouter(options));
+
+    // Serve the Swagger documents and Swagger UI
+    app.use(middleware.swaggerUi());
+
+});
 
 // Error handling middleware
 app.use(function(err, req, res, next) {
